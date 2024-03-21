e87068c692c301e99c5f9bf43a79de86e881d921
Shivani <pathakshivani843@gmail.com>
2024-03-21 22:50:34 +0530

    cards added with content

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,35 @@ import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 
+//for cards 
+// import { Products } from './components/products';
+//  import contents from './content';
+
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
   return (
+    <>
     <div className='w-full min-h-screen text-white bg-zinc-900'>
     <Navbar/>
     <LandingPage/>
     <Marquee/>
     </div>
+  
+  {/* for cards */}
+  {/* <div className='App grid grid-cols-4 justify-center  bg-zinc-900'>
+                {contents.map(contents => (
+                    <Products 
+                        key={contents.id}
+                        image={contents.image}
+                        name={contents.name}
+                        price={contents.price}
+                        totalSales={contents.totalSales}
+                        discription={contents.discription}
+                        rating={contents.rating}
+                    />
+                ))}
+            </div> */}
+</>
   )
 }
 
